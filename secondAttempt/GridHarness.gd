a7fe16d1270739b1c7a628437f19f550542df176
Joe OpenSrc <joe-opensrc@protonmail.ch>
2023-11-02 23:12:22 +0100

    blob<:

--- secondAttempt/GridHarness.gd
+++ secondAttempt/GridHarness.gd
@@ -1,3 +1,14 @@
 extends Control
 
-@export var aBox: PackedScene = preload("res://secondAttempt/ABox/a_box.tscn")
+
+var aBox: PackedScene = preload("res://secondAttempt/ABox/a_box.tscn")
+@export var openboxTex: Texture2D = preload("res://secondAttempt/ABox/icon.svg")
+@export  var gridSize: int = 3
+
+func _ready():
+  %TheGrid.set_columns(gridSize)
+  for i in range(gridSize * gridSize):
+    print(i)
+    var anotherBox = aBox.instantiate()
+    %TheGrid.add_child(anotherBox)
+    print_tree_pretty()
