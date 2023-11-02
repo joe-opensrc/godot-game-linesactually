75165df83ea26f177060a3f029ed96b3bf3269b4
Joe OpenSrc <joe-opensrc@protonmail.ch>
2023-11-03 00:07:32 +0100

    nice grid of buttons

--- secondAttempt/GridHarness.gd
+++ secondAttempt/GridHarness.gd
@@ -1,8 +1,7 @@
 extends Control
 
-
 var aBox: PackedScene = preload("res://secondAttempt/ABox/a_box.tscn")
-@export var openboxTex: CompressedTexture2D = preload("res://secondAttempt/ABox/icon.svg")
+@export var openBoxTexture: CompressedTexture2D = preload("res://secondAttempt/ABox/openbox.svg")
 @export  var gridSize: int = 3
 
 func _ready():
@@ -11,6 +10,10 @@ func _ready():
     print(i)
     var anotherBox = aBox.instantiate()
     anotherBox.set_name("ABox" + str(i))
-    anotherBox.set_texture(openboxTex)
+    anotherBox.set_texture(openBoxTexture.duplicate(true))
+    anotherBox.connect("pressed",_on_box_pressed)
     %TheGrid.add_child(anotherBox)
     print_tree_pretty()
+
+func _on_box_pressed():
+  print("woot")
