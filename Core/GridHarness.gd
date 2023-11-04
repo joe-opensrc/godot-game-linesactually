d65a8b22942b86f690ac10e588957e22adcb3a4d
Joe OpenSrc <joe-opensrc@protonmail.ch>
2023-11-04 17:28:03 +0100

    adds vars; some specifiable, some calculated
    noOfCols * noOfCols == totalNumOfCards
    memGroupSize:   number of cards within each group the player must find
    minNumTextures: used to visually distinguish between groups
    However, there has to be a better way. :O
    Tis ain't Doom-lvl code! :D

--- Core/GridHarness.gd
+++ Core/GridHarness.gd
@@ -1,15 +1,33 @@
 extends Control
 
-@export  var gridSize: int = 3
+@export var        noOfCols: int = 2
+@export var        noOfRows: int = 2
+@export var    memGroupSize: int = 2
+var         totalNumOfCards: int
+var          minNumOfTextures: float
 
 const aBox: PackedScene = preload("res://Core/ABox/a_box.tscn")
 @export var   openBoxTexture: CompressedTexture2D = preload("res://Core/ABox/openbox.svg")
 @export var closedBoxTexture: CompressedTexture2D = preload("res://Core/ABox/closedbox.svg")
 
-func _ready():
-  %TheGrid.set_columns(gridSize)
-  for i in range(gridSize * gridSize):
 
+func _init():
+  totalNumOfCards = noOfRows * noOfCols
+  # there has to be a better way! :O
+  # this ain't Doom! :D
+  minNumOfTextures = totalNumOfCards / float(memGroupSize)
+  assert( fmod( minNumOfTextures, (totalNumOfCards / memGroupSize) ) == 0.0 )
+
+
+#  assert(memGroupSize > 1)
+#  assert(minNumTextures > 1 && minNumTextures != totalNumOfCards)
+
+func _ready():
+  pass
+
+  %TheGrid.set_columns(noOfCols)
+
+  for i in range(totalNumOfCards):
     var anotherBox = aBox.instantiate()
     anotherBox.set_name("ABox" + str(i))
     anotherBox.set_texture(openBoxTexture)
